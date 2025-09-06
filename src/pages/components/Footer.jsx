096f55728635171362829290f46380ac5cc9950d
FloeXs <142821557+FloeXs@users.noreply.github.com>
2025-09-07 01:41:33 +0300

    Update Footer.jsx

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import { LuMail } from 'react-icons/lu';
 import { FaWhatsapp, FaSpotify } from "react-icons/fa";
-import { MdOutlineMail } from "react-icons/md";
-import { FaLinkedin } from 'react-icons/fa6';
 
 
 const Footer = () => {
   return (
     <div className='flex flex-col-reverse items-center justify-between p-5 border-zinc-200 dark:border-zinc-600 sm:flex-row sm:gap-y-0 border-t border-white'>
         <ul className='dark:text-white flex gap-4'>
-          <li><a target='_blank' href="https://wa.me/+905550114185"><FaWhatsapp className='text-2xl' /></a></li>
-          <li><a target='_blank' href="mailto:efeturkerr0@gmail.com"><FaWhatsapp className='text-2xl' /></a></li>
-          <li><a target='_blank' href="https://open.spotify.com/user/x7ysqyt66hp91dazp9mg593tk?si=56332ca1c29649d6"><FaSpotify className='text-2xl' /></a></li>
+          <li><a rel='noreferrer' target='_blank' href="https://wa.me/+905550114185"><FaWhatsapp className='text-2xl' /></a></li>
+          <li><a rel='noreferrer' target='_blank' href="mailto:efeturkerr0@gmail.com"><FaWhatsapp className='text-2xl' /></a></li>
+          <li><a rel='noreferrer' target='_blank' href="https://open.spotify.com/user/x7ysqyt66hp91dazp9mg593tk?si=56332ca1c29649d6"><FaSpotify className='text-2xl' /></a></li>
         </ul>
         <div className="flex items-center gap-3">
           <div className="flex rounded-full border border-zinc-600 p-1.5"><LuMail className='dark:text-white' /></div>
