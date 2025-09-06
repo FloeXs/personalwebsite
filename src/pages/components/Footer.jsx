37172ce9c20faff88317b6d75c2f2dbd992e920e
FloeXs <142821557+FloeXs@users.noreply.github.com>
2025-09-07 01:48:01 +0300

    Update Footer.jsx

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className='flex flex-col-reverse items-center justify-between p-5 border-zinc-200 dark:border-zinc-600 sm:flex-row sm:gap-y-0 border-t border-white'>
         <ul className='dark:text-white flex gap-4'>
           <li><a rel='noreferrer' target='_blank' href="https://wa.me/+905550114185"><FaWhatsapp className='text-2xl' /></a></li>
-          <li><a rel='noreferrer' target='_blank' href="mailto:efeturkerr0@gmail.com"><FaWhatsapp className='text-2xl' /></a></li>
+          <li><a rel='noreferrer' target='_blank' href="mailto:efeturkerr0@gmail.com"><LuMail className='text-2xl' /></a></li>
           <li><a rel='noreferrer' target='_blank' href="https://open.spotify.com/user/x7ysqyt66hp91dazp9mg593tk?si=56332ca1c29649d6"><FaSpotify className='text-2xl' /></a></li>
         </ul>
         <div className="flex items-center gap-3">
